bd1d11443a9e7330b64c488fcf7e56cfa719e9fb
Tulasi <lakshmitulasitellakula@gmail.com>
2023-07-23 00:45:04 +0530

    added changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-
-
+import "./App.css";
+import Navbar from "./components/navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
     <div className="App">
-     
+      {Navbar()}
+      {/* <Navbar/>   ===> same as above(other syntax) */}
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,24 @@
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+      <div class="logo">MUO</div>
+      <ul class="nav-links">
+        <div class="menu">
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+          <li>
+            <a href="logout">Logout</a>
+          </li>
+        </div>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
